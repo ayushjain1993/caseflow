204076abf5330d7a73b76525e1f434ddb0e3e085
Andrew Lomax <andrewlomaxr@gmail.com>
2018-11-13 15:20:04 -0500

    add fetch_days_with_open_slots query

--- app/repositories/hearing_day_repository.rb
+++ app/repositories/hearing_day_repository.rb
@@ -40,6 +40,69 @@ class HearingDayRepository
       [video_and_co.uniq { |hearing_day| [hearing_day[:hearing_date].to_date, hearing_day[:room_info]] }, travel_board]
     end
 
+    def fetch_days_with_open_hearings_slots(start_date, end_date)
+      # replaces slots_based_on_type, HearingDay.filter_non_scheduled_hearings, and
+      # HearingDay.load_days_with_open_hearing_slots logic
+
+      # see HearingRepository.fetch_video_hearings_for_parent /
+      # CaseHearing.video_hearings_for_master_record
+      matches_by_key = "h.vdkey = hearsched.hearing_pkseq"
+      # see HearingRepository.fetch_co_hearings_for_parent /
+      # CaseHearing.co_hearings_for_master_record
+      matches_by_date = "(
+        h.hearing_type = 'C' AND
+        h.folder_nr NOT LIKE '%VIDEO%' AND
+        trunc(h.hearing_date) = trunc(hearsched.hearing_date)
+      )"
+      # see HearingDay.filter_non_scheduled_hearings
+      is_scheduled = "(
+        (h.hearing_type = 'C' AND h.folder_nr IS NOT NULL) OR
+        (h.hearing_disp NOT IN ('P', 'C'))
+      )"
+
+      slotted_hearings = "(
+         SELECT COUNT(*)
+         FROM vacols.hearsched h
+         WHERE (#{matches_by_key} OR #{matches_by_date}) AND #{is_scheduled}
+      )"
+      # fetch_hearing_day_slots + slots_based_on_type
+      total_slots = "
+        CASE WHEN hearing_type = 'C' THEN 11
+             WHEN hearing_type = 'V' THEN s.stc4
+             WHEN hearing_type = 'T' AND to_char(hearing_date, 'day') IN ('monday', 'friday') THEN s.stc2
+             ELSE s.stc3
+        END
+      "
+
+      days_for_range = "
+        trunc(hearing_date) BETWEEN '#{VacolsHelper.day_only_str(start_date)}' AND '#{VacolsHelper.day_only_str(end_date)}'
+      "
+
+      days_for_central_office = "(
+        hearing_type = 'C' AND
+        folder_nr NOT LIKE '%VIDEO%' AND
+        trunc(hearing_date) BETWEEN '#{VacolsHelper.day_only_str(start_date)}' AND '#{VacolsHelper.day_only_str(end_date)}'
+      )"
+
+      VACOLS::CaseHearing.select_schedule_days
+        .select("#{slotted_hearings} AS slotted_hearings, #{total_slots} AS total_slots")
+        .joins("left outer join vacols.staff s ON s.stafkey = folder_nr")
+        .where("#{slotted_hearings} < (#{total_slots}) AND (#{days_for_central_office} OR #{days_for_range})")
+    end
+
+    def load_days_with_open_hearings_slots(start_date, end_date)
+      hearing_days = fetch_days_with_open_hearings_slots(start_date, end_date)
+      hearings_by_day = HearingRepository.fetch_hearings_for_parents(hearing_days).group_by { | h | h[:hearing_pkseq] }
+
+      hearing_days = hearing_days.each_with_object([]) do |_day, result|
+        day = to_canonical_hash(_day)
+        day[:hearings] = hearings_by_day[day[:hearing_pkseq]]
+        result << day
+      end
+
+      hearing_days.uniq { | day | [day[:hearing_date].to_date, day[:room_info]] }
+    end
+
     def load_days_for_regional_office(regional_office, start_date, end_date)
       video_and_co = VACOLS::CaseHearing.load_days_for_regional_office(regional_office, start_date, end_date)
         .each_with_object([]) do |hearing, result|
